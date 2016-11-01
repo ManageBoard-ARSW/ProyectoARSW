2692b1ea7e064ab03aefb51f6c6b20275c993cbf
sebastianamaya <sebastian.amaya@mail.escuelaing.edu.co>
2016-10-31 23:34:29 -0500

    vista tablero

---
+++ src/main/java/edu/eci/arsw/manageboard/services/Manejador.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.manageboard.services;
+
+import edu.eci.arsw.manageboard.logic.Tablero;
+import edu.eci.arsw.manageboard.logic.Tarea;
+import java.util.LinkedList;
+import java.util.List;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ *
+ * @author Owner
+ */
+
+public class Manejador {
+    List<Tarea> tareas;
+
+    public List<Tarea> getTareas() {
+        return tareas;
+    }
+
+    public Manejador (){
+        tareas = new LinkedList<>();
+        cargarTareas(this);
+    
+}
+
+    private void cargarTareas(Manejador mt) {
+       Tarea t = new Tarea("HACER PROYECTO", 0);
+       mt.registrarTarea(t);
+       Tarea t1 = new Tarea("HACER PROYECTO1", 1);
+       mt.registrarTarea(t);
+       Tarea t2 = new Tarea("HACER PROYECTO2", 2);
+       mt.registrarTarea(t);
+       Tarea t3 = new Tarea("HACER PROYECTO3", 3);
+       mt.registrarTarea(t);
+       
+    }
+
+    private void registrarTarea(Tarea t) {
+    tareas.add(t);
+    }
+}
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,43 @@
 var stompClient = null;
 var puntos=[];
+/**
+var tareasDisp=[];
+actualizarTareasDisponibles = function(){
+    funtion_get_all_tasks().then(funcionP).then(function(){
+        $("#contenido").droppable({
+            drop: function (event, ui) {
+            var idElementoSoltado=ui.draggable.attr("id");
+                agregarTareaProyecto()(idElementoSoltado);
+              }
+            });
+          
+        $("li").draggable({
+        helper: 'clone',
+        containment: 'document',
+        revert: true
+        });
+    });
+};
 
+    funtion_get_all_tasks=function(){
+          return $.get("/tablero").then(function(task){
+
+            tareasDispDisp=plates;
+    });
+  };
+
+
+    funcionP=function(){
+        for (i = 0; i < tareasDisp.length; i++){
+            $("#tareas").append("<li id="+i+" class='draggable' >"+tareasDisp[i].nombre+"</li>");
+      }
+
+  };
+  
+     agregarTareaProyecto= function (p){
+        p=new Tarea(tareasDisp[p].nombre,tareasDisp[p].idTarea);
+  };
+ */
 function connect() {
     var socket = new SockJS('/stompendpoint');
     stompClient = Stomp.over(socket);
